68505982858e3f4215ce557ccba91b39f37df574
John Shih <>
2024-03-17 17:53:12 +1000

    Updated other image list

--- src/components/Projects/components/Others.tsx
+++ src/components/Projects/components/Others.tsx
@@ -7,12 +7,12 @@ import { otherProjectData } from "../../../assets/mock/data";
 import { ProjectItem } from "../../../types";
 import { BsWindow } from "react-icons/bs";
 
-import otherImg1 from "../../../assets/images/projects/other/rib-software.jpg";
-import otherImg2 from "../../../assets/images/projects/other/itwocostx.jpg";
-import otherImg3 from "../../../assets/images/projects/other/itwocx.jpg";
-import otherImg4 from "../../../assets/images/projects/other/tmdb.jpg";
-import otherImg5 from "../../../assets/images/projects/other/techweb.jpg";
-import otherImg6 from "../../../assets/images/projects/other/program.jpg";
+import otherImg1 from "../../../assets/images/projects/other/nx-witness.jpg";
+import otherImg2 from "../../../assets/images/projects/other/rib-software.jpg";
+import otherImg3 from "../../../assets/images/projects/other/itwocostx.jpg";
+import otherImg4 from "../../../assets/images/projects/other/itwocx.jpg";
+import otherImg5 from "../../../assets/images/projects/other/tmdb.jpg";
+import otherImg6 from "../../../assets/images/projects/other/techweb.jpg";
 
 interface ProjectImage {
   otherImg1: string;
